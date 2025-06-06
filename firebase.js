29db2a6c8da7f4579b585d8120302aad28474562
Caden <cadencrowson@gmail.com>
2025-06-05 20:07:52 -0500

    Fix subscription form integration

--- firebase.js
+++ firebase.js
@@ -1,7 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Assumes the compat SDKs are loaded via script tags in index.html.
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,8 +11,8 @@ const firebaseConfig = {
     appId: "1:818576769248:web:e3ec53708bc4e40d5a5085"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase using the global `firebase` object provided by the compat SDKs
+const app = firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
 document.getElementById("subscribe-form").addEventListener("submit", function (e) {
