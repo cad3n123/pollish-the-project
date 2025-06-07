e61d9ae95a3b910b88ca938a01179f2ab53c4f16
cad3n123 <cadencrowson@gmail.com>
2025-06-07 14:34:58 -0500

    Made scroll quicker

--- script.js
+++ script.js
@@ -233,7 +233,7 @@ function closeNewsletterForm() {
 }
 document.getElementById('logo').addEventListener('click', () => {
   const start = window.scrollY;
-  const duration = 400; // faster scroll back to top
+  const duration = 200; // faster scroll back to top
   const startTime = performance.now();
 
   function scrollStep(timestamp) {
