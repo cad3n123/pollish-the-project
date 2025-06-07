8e4a880e463e6ac39ed83b0edec831f08e78314b
cad3n123 <cadencrowson@gmail.com>
2025-06-06 21:40:04 -0500

    Added slight fade in on page open

--- script.js
+++ script.js
@@ -198,6 +198,9 @@ document.addEventListener('DOMContentLoaded', () => {
     if (slides[1]) {
         slides[1].addEventListener('click', playRareroom);
     }
+    setTimeout(() => {
+        document.getElementById('curtain').classList.remove('active');
+    }, 0);
 });
 function setCountryList() {
     fetch("https://restcountries.com/v3.1/all?fields=name")
